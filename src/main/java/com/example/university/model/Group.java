bba4f1f918333be3c3cc2a19dcb91b33a7965c97
Niyaz Kadirov <niyaz.kadirov@yandex.ru>
2020-02-25 12:12:25 +0300

    optimize import

--- src/main/java/com/example/university/model/Group.java
+++ src/main/java/com/example/university/model/Group.java
@@ -1,8 +1,6 @@
 package com.example.university.model;
 
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,7 +11,7 @@ import java.util.Set;
 @Getter
 @Setter
 @Entity
-@Table(name="\"group\"")
+@Table(name = "\"group\"")
 public class Group {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
