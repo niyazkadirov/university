ceffd0adb66408f10969ce73b9b5bcccced48857
Niyaz Kadirov <niyaz.kadirov@yandex.ru>
2020-02-28 13:39:51 +0300

    Refactoring test (Mockito)

--- src/test/java/com/example/university/service/StudentServiceImplTest.java
+++ src/test/java/com/example/university/service/StudentServiceImplTest.java
@@ -2,38 +2,43 @@ package com.example.university.service;
 
 import com.example.university.entity.Student;
 import com.example.university.repository.StudentRepository;
+import org.aspectj.lang.annotation.Before;
 import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 class StudentServiceImplTest {
 
-    @Mock
-    private StudentRepository studentRepository;
+    private StudentRepository mockedStudentRepository = mock(StudentRepository.class);
 
-    @InjectMocks
+
     private StudentServiceImpl studentService;
-    
+
+
+    @BeforeEach
+    public void setUp() {
+        studentService = new StudentServiceImpl(mockedStudentRepository);
+    }
 
     @Test
     void addedStudentMustBeNotNull() {
         Student student = new Student();
         student.setFirstName("Bob");
+        when(mockedStudentRepository.save(Mockito.any(Student.class))).thenReturn(student);
 
-        when(studentRepository.save(Mockito.any(Student.class))).thenReturn(student);
         Student savedStudent = studentService.addStudent(student);
 
         Assert.assertNotNull(savedStudent.getFirstName());
         Assert.assertEquals(savedStudent.getFirstName(), "Bob");
 
-        Mockito.verify(studentRepository, Mockito.times(1)).save(student);
+        Mockito.verify(mockedStudentRepository, Mockito.times(1)).save(student);
     }
 
 
