48ade306dc4125d78e1f40c4e894bebdf15b1036
Niyaz Kadirov <niyaz.kadirov@yandex.ru>
2020-03-03 17:37:15 +0300

    init

---
+++ src/main/java/com/example/university/entity/Student.java
@@ -0,0 +1,43 @@
+package com.example.university.entity;
+
+import com.example.university.entity.enumeration.Gender;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "gender")
+    private Gender gender;
+    @Column(name = "birth_date", columnDefinition = "date")
+    private LocalDate birthDate;
+
+    @ManyToOne()
+    @JoinColumn(name = "group_id")
+    private Group group;
+
+
+    public Student(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
