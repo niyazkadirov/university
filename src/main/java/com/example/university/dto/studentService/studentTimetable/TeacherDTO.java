f64b44fe9762906c9e61b2a596f665aa12a23acb
Jmokut <joshuamokut@gmail.com>
2020-03-06 20:54:15 +0300

    What? I didn't do anything

--- src/main/java/com/example/university/dto/studentService/studentTimetable/TeacherDTO.java
+++ src/main/java/com/example/university/dto/studentService/studentTimetable/TeacherDTO.java
@@ -3,8 +3,6 @@ package com.example.university.dto.studentService.studentTimetable;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-
 @Getter
 @Setter
 public class TeacherDTO {
