53840389a98447695fff6a6e2dc192d28fce957b
Jmokut <joshuamokut@gmail.com>
2020-03-06 20:04:38 +0300

    Included allArgsconstructor

--- src/main/java/com/example/university/dto/StudentDTO.java
+++ src/main/java/com/example/university/dto/StudentDTO.java
@@ -1,8 +1,10 @@
 package com.example.university.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+@AllArgsConstructor
 @Getter
 @Setter
 public class StudentDTO {
