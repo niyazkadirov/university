ee1e8f39963f3b45f42eae69f1aaf7517cf65e7b
Jmokut <joshuamokut@gmail.com>
2020-03-06 20:58:13 +0300

    restructuring

--- src/main/java/com/example/university/Mappers/LectureMapper.java
+++ src/main/java/com/example/university/Mappers/LectureMapper.java
@@ -1,24 +1,28 @@
 package com.example.university.Mappers;
 
-import com.example.university.dto.LectureDTO;
+import com.example.university.dto.studentService.studentTimetable.SubjectDTO;
+import com.example.university.dto.teacherService.teacherTimeTable.LectureDTO;
 import com.example.university.entity.Lecture;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 
-import java.time.Duration;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@AllArgsConstructor
 public class LectureMapper {
+    private static ModelMapper modelMapper = new ModelMapper();
 
-
-    public static LectureDTO mapLectureToDTO(Lecture lecture){
-        return new LectureDTO(lecture.getSubject().getTitle(), lecture.getStartTime(), lecture.getEndTime(),
-                Duration.between(lecture.getStartTime(), lecture.getEndTime()),
+    public static LectureDTO mapLectureToDTO(Lecture lecture) {
+        return new LectureDTO(modelMapper.map(lecture.getSubject(), SubjectDTO.class), lecture.getStartTime(), lecture.getEndTime(),
+                lecture.getAudienceNumber(),
                 StudentMapper.mapStudentsToDTOList(lecture.getGroups().stream().
-                        flatMap(group -> group.getStudents().stream()).collect(Collectors.toList()))
-                );
+                        flatMap(group -> group.getStudents().stream()).collect(Collectors.toList())),
+                lecture.getDay()
+        );
     }
 
-    public static List<LectureDTO> mapLectureToDTOList(List<Lecture> lectures){
+    public static List<LectureDTO> mapLectureToDTOList(List<Lecture> lectures) {
         return lectures.stream().map(LectureMapper::mapLectureToDTO).collect(Collectors.toList());
     }
 }
