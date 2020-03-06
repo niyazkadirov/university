7f956bc1e67181a296f4acb8b731bcae1c1b27d7
Niyaz Kadirov <niyaz.kadirov@yandex.ru>
2020-03-06 11:26:24 +0300

    Deleted sorted by day from student timetable

--- src/main/java/com/example/university/service/StudentServiceImpl.java
+++ src/main/java/com/example/university/service/StudentServiceImpl.java
@@ -87,11 +87,7 @@ public class StudentServiceImpl implements StudentService {
         }
         Map<Day, List<LectureDTO>> lectureDTOMap =
                 lectureDTOList.stream()
-                        .collect(Collectors.groupingBy(LectureDTO::getDay))
-                        .entrySet().stream()
-                        .sorted(Map.Entry.comparingByKey())
-                        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
-                                (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+                        .collect(Collectors.groupingBy(LectureDTO::getDay));
 
         studentDTO.setLectureDTOMap(lectureDTOMap);
 
